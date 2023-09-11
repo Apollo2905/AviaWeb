e1d5d209caca93a2d8985e482fd35b12880f951c
MairaAllen <kalashnikovalen79@mail.ru>
2023-09-11 22:27:30 +0300

    Creating the main application.

---
+++ aviaweb/main/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home/', include(('aviaweb.urls', 'aviaweb'), namespace='aviaweb'))]
